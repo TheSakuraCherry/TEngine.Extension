5571faf323c753aa0bd1c74d0015cf7ca2c53a00
ALEXTANGXIAO <574809918@qq.com>
2025-03-21 00:27:02 +0800

    更新TEngine的拓展Bulgly

---
+++ Bugly/Assets/Scripts/Bugly.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace TEngine
+{
+    public class Bugly:MonoBehaviour
+    {
+        private void Awake()
+        {
+#if UNITY_IPHONE || UNITY_ANDROID
+            BuglyManager.Instance.Init(Resources.Load<BuglyConfig>("BuglyConfig"));
+            Destroy(this.gameObject);
+#endif
+        }
+    }
+}
